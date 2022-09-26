8e7fbce9f4bcf22798bbfc8beddb4d726b23c853
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-09-26 21:58:59 +0300

    Update Array plus array(8kyu).js

--- Array plus array(8kyu).js
+++ Array plus array(8kyu).js
@@ -2,9 +2,6 @@ function arrayPlusArray(arr1, arr2) {
 let sum = 0;
     for (let i = 0; i < arr1.length; i++){
       sum += arr1[i];
-      }
-      for(let i = 0; i < arr2.length; i++){
+  } for(let i = 0; i < arr2.length; i++){
         sum += arr2[i];
-        }
-  return sum; 
-}
+  } return sum; }
