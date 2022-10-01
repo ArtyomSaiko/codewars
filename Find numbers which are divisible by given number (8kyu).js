829e3646f0b9b3078850ed945068b4bf1ebe9a69
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-01 21:04:56 +0300

    Update Find numbers which are divisible by given number (8kyu).js

--- Find numbers which are divisible by given number (8kyu).js
+++ Find numbers which are divisible by given number (8kyu).js
@@ -1,3 +1,17 @@
+//first way
+
 function divisibleBy(numbers, divisor){
   return numbers.filter((number2) => number2 % divisor === 0)
   }
+
+//second way
+
+function divisibleBy(numbers, divisor) {
+  var arr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      arr.push(numbers[i]);
+    } 
+  }
+return arr;
+}
