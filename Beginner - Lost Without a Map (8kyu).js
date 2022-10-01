7f2f588eb41a1b7710b9fd7c8dd578102bdcfc6c
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-01 22:15:30 +0300

    Update Beginner - Lost Without a Map (8kyu).js

--- Beginner - Lost Without a Map (8kyu).js
+++ Beginner - Lost Without a Map (8kyu).js
@@ -1,3 +1,16 @@
+\\ first way
+
 function maps(x){
    return x.map(n => n * 2);
+}
+
+
+\\second way
+
+function maps(x){
+let arr = [];
+for (let i = 0; i < x.length; i++){
+arr.push(x[i] * 2);
+}
+return arr;
 }
