36c50320aa0bcce2e33bbbd4a7501da5e8be8ad2
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-02 12:39:20 +0300

    Update Convert boolean values to strings 'Yes' or 'No'(8kyu).js

--- Convert boolean values to strings 'Yes' or 'No'(8kyu).js
+++ Convert boolean values to strings 'Yes' or 'No'(8kyu).js
@@ -1,6 +1,12 @@
+\\first way
 function boolToWord( bool ){
   if (bool == true){
     return "Yes";
   } else 
     return "No";
+}
+
+\\second way
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
 }
