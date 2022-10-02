b37858b8add1b990845d94ee1056929589af8d63
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-02 12:04:02 +0300

    Update Counting sheep (8kyu).js

--- Counting sheep (8kyu).js
+++ Counting sheep (8kyu).js
@@ -1,7 +1,19 @@
+\\first way
+
 function countSheeps(arrayOfSheep) {
   let amount = 0;
   for (let i = 0; i <= arrayOfSheep.length; i++){
     if(arrayOfSheep[i]) amount += 1
     }
   return amount;
+}
+
+\\second way
+
+function countSheeps(arrayOfSheep) {
+  var amount = 0;
+    for(var i = 0; i < arrayOfSheep.length; i++)
+    if(arrayOfSheep[i] == true)
+      amount++;
+    return amount;
 }
