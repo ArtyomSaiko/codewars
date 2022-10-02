2d5c617559e82ce3fc9fb5b89ec21b369ede93e4
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-02 12:12:37 +0300

    Update Is it even (8kyu).js

--- Is it even (8kyu).js
+++ Is it even (8kyu).js
@@ -1,5 +1,11 @@
+\\ first way
 function testEven(n) {
     if (n % 2 === 0) {
       return true;
     } else return false;
+}
+
+\\ second way
+function testEven(n) {
+  return n % 2 === 0 ? true : false;
 }
