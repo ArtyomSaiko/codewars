317dfa207c243255219514a70cc2106672799763
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-03 21:25:55 +0300

    Update Calculate average (8kyu).js

--- Calculate average (8kyu).js
+++ Calculate average (8kyu).js
@@ -1,2 +1,15 @@
+\\first way
 const findAverage = array => 
   array.length > 0 ? array.reduce ((a, b) => a + b)/array.length : 0
+
+\\second way
+function find_average(array) {
+  if (array.length === 0) {
+  return 0;
+  }
+  var a = 0;
+  for (i=0; i<array.length; i++) {
+    a +=array[i];
+  }
+  return a/array.length;
+}
