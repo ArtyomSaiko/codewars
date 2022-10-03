2ab8f1c0c63fa9661ab519d2d87db06ea632697a
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-03 21:00:03 +0300

    Update My head is at the wrong end (8kyu).js

--- My head is at the wrong end (8kyu).js
+++ My head is at the wrong end (8kyu).js
@@ -1,3 +1,13 @@
+\\first way
 function fixTheMeerkat(arr) {
  return arr.reverse();
+}
+
+\\second way
+function fixTheMeerkat(arr) {
+  var end = arr.shift();
+  var begin = arr.pop();
+arr.push(end);
+arr.unshift(begin);
+  return arr; 
 }
