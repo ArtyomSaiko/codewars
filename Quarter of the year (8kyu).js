610f8a2561f7db25b0ab2c8d97acf080839d19a3
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-03 21:09:55 +0300

    Update Quarter of the year (8kyu).js

--- Quarter of the year (8kyu).js
+++ Quarter of the year (8kyu).js
@@ -1,6 +1,21 @@
+\\first way
 const quarterOf = (month) => {
  if(month < 4) return 1;
  if(month < 7) return 2;
  if(month < 10) return 3;
  return 4;
+}
+
+\\second way
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1
+  } else if (month <= 6) {
+    return 2
+  } else if (month <= 9) {
+    return 3
+  } else if (month <= 12) {
+    return 4
+  }
+  
 }
