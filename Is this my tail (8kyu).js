e260cf4c45f02ced79acdd887231f6d2797f564b
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-04 21:48:26 +0300

    Update Is this my tail (8kyu).js

--- Is this my tail (8kyu).js
+++ Is this my tail (8kyu).js
@@ -1,1 +1,13 @@
+\\first way
 correctTail = (bod, tail) =>bod[bod.length-1] == tail
+
+\\second way
+function correctTail(body, tail) {
+   var sub = body.substr(body.length-(tail.length));
+   if (sub === tail) {
+   return true;
+  }
+  else {
+    return false;
+  }
+}
