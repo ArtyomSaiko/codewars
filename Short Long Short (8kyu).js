ee5ad554eb1c7fac75d4571bd633d43c43921d6b
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-04 20:28:25 +0300

    Update Short Long Short (8kyu).js

--- Short Long Short (8kyu).js
+++ Short Long Short (8kyu).js
@@ -1,5 +1,15 @@
+\\first way
 function solution(a, b){
   let short = a.length < b.length ? a : b;
   let long = a.length > b.length ? a : b;
   return short + long + short;
+}
+
+\\second way
+function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
 }
