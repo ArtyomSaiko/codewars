e03dd43fb9935b9fbcf30387bfbf5177f7223fdf
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-05 21:13:22 +0300

    Update Jenny's secret message (8kyu).js

--- Jenny's secret message (8kyu).js
+++ Jenny's secret message (8kyu).js
@@ -1,5 +1,11 @@
+\\first way
 function greet(name){
   if(name === "Johnny"){
     return "Hello, my love!";
 } else return "Hello, " + name + "!";
+}
+
+\\second way
+function greet(name){
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
 }
