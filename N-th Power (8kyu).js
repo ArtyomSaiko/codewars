2a5628c8ebee55175bca61e0c300bef43a5aaf40
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-05 20:15:00 +0300

    Update N-th Power (8kyu).js

--- N-th Power (8kyu).js
+++ N-th Power (8kyu).js
@@ -1,3 +1,4 @@
+\\ first way
 function index(array, n){
   if (array[n] == undefined) { 
     return -1;
@@ -5,3 +6,6 @@ function index(array, n){
     return Math.pow(array[n], n);
   }
 }
+
+\\ second way
+const index = (array, n) => array.length > n ? Math.pow(array[n], n) : -1;
