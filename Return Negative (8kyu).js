273429732d8b853566168bc79ce963c24bc11e06
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-05 20:36:53 +0300

    Update Return Negative (8kyu).js

--- Return Negative (8kyu).js
+++ Return Negative (8kyu).js
@@ -1,5 +1,11 @@
+\\sfirst way
 function makeNegative(num) {
   if (num > 0) {
     return num * (-1);
   } else return num;
+}
+
+\\second way
+function makeNegative(num) {
+  return num < 0 ? num : -num;
 }
