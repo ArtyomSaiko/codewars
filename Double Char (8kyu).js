611dd2137a961f46ffb90f76a53b95cd94d4465e
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-05 20:33:52 +0300

    Update Double Char (8kyu).js

--- Double Char (8kyu).js
+++ Double Char (8kyu).js
@@ -1,3 +1,13 @@
+\\first way
 function doubleChar(str) {
   return str.replace(/./g, '$&$&');
 }
+
+\\second way
+function doubleChar(str) {
+  var a = '';
+  for (var i = 0; i < str.length; i++){
+    a = a + str[i] + str[i];
+  };
+  return a;
+};
