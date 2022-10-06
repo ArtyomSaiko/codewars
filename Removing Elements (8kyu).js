9757e6d11d2fbcb69a21c732b00dc40960ecc70a
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-06 20:40:17 +0300

    Update Removing Elements (8kyu).js

--- Removing Elements (8kyu).js
+++ Removing Elements (8kyu).js
@@ -1,3 +1,4 @@
+\\first way
 function removeEveryOther(arr){
 let count = 0
 for(let i=0; i<arr.length; i++ ){
@@ -6,4 +7,13 @@ if(count%1===0) {arr.splice(i+1,1)}
   else {null}
 } 
   return arr
+}
+
+\\second way
+function removeEveryOther(arr){
+  var newArr=[];
+for (var i = 0; i < arr.length; i+=2){
+  newArr.push(arr[i]);
+  }
+return newArr;
 }
