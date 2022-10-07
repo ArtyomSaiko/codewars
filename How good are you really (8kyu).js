3e10c73e962256d139dd28b177cd79a480c912d1
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-07 21:32:41 +0300

    Update How good are you really (8kyu).js

--- How good are you really (8kyu).js
+++ How good are you really (8kyu).js
@@ -2,3 +2,13 @@
 function betterThanAverage(classPoints, yourPoints) {
   return classPoints.reduce((acc, item) => acc + item, 0) / classPoints.length < yourPoints;
   }
+
+\\second way
+function betterThanAverage(classPoints, yourPoints) {
+  var A = 0;
+  for (var i = 0; i < classPoints.length; i++){
+    A += classPoints[i]; 
+  }
+  A = A/classPoints.length; 
+  return yourPoints > A;
+}
