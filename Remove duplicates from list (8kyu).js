dcf403b457b64ef4b62dc1620794cdce286d3a03
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-07 20:54:53 +0300

    Update Remove duplicates from list (8kyu).js

--- Remove duplicates from list (8kyu).js
+++ Remove duplicates from list (8kyu).js
@@ -1,9 +1,14 @@
+\\first way
 function distinct(a) {
  var newArr = [];
     for (var i = 0; i < a.length; i++) {
         if (newArr.indexOf(a[i]) === -1) {
             newArr.push(a[i]);
         }
-    }
-  return newArr;
+    }  return newArr;
+}
+
+\\second way
+function distinct(a) {
+  return Array.from(new Set(a));
 }
