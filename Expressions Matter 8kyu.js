e5b6cc9d9fa6b988f0588e0105511d9c50855ab5
Artyom <artem.sok27@gmail.com>
2022-10-09 17:16:55 +0300

    second way

--- Expressions Matter 8kyu.js
+++ Expressions Matter 8kyu.js
@@ -1,3 +1,4 @@
+\\ first way
 function expressionMatter(a, b, c) {
     let output = [];
     output.push(a *(b + c));
@@ -6,4 +7,9 @@ function expressionMatter(a, b, c) {
     output.push((a + b) * c);
     output.push(a+b+c);
       return Math.max(...output);
+  }
+
+  \\ second way
+  function expressionMatter(a, b, c) {
+    return Math.max(a+b+c, a*b*c, (a+b)*c, a*(b+c));
   }
