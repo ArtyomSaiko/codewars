30a1f7367d0b12dc734ef9786a80d9f004c43730
Artyom <artem.sok27@gmail.com>
2022-10-09 19:36:21 +0300

    ;

--- Student's Final Grade (8kyu).js
+++ Student's Final Grade (8kyu).js
@@ -30,13 +30,13 @@ const finalGrade = (exam, projects) => {
   finalGrade = (exam, projects) => {
     switch (true) {
          case (exam > 90 || projects > 10):
-         return 100
+         return 100;
          break;
          case (exam > 75 && projects >= 5):
          return 90;
          break;
          case (exam > 50 && projects >= 2):
-         return 75
+         return 75;
          break;
          case (exam <= 55 || projects <= 2):
          return 0;
