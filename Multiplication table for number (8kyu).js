89922760bc964783a966fea7e8ff6b17be8f91a4
ArtyomSaiko <112972829+ArtyomSaiko@users.noreply.github.com>
2022-10-09 15:19:07 +0300

    Update Multiplication table for number (8kyu).js

--- Multiplication table for number (8kyu).js
+++ Multiplication table for number (8kyu).js
@@ -1,8 +1,16 @@
+\\first way
 function multiTable(number) {
   let res = '';
   for(let i = 1; i <= 10; i++){
     res += `${i} * ${number} = ${i * number}`;
     if(i < 10)  res +='\n';
-  }
-  return res;
+  } return res;
+}
+
+\\second way
+function multiTable(number) {
+  let table = '';
+  for(let i = 1; i <= 10; i++){
+    table += `${i} * ${number} = ${i*number}\n`;
+  } return table.slice(0, -1);
 }
