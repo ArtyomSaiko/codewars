62ce9b121ede21056e1ed4862fe2093a2b823819
Artyom <artem.sok27@gmail.com>
2022-10-09 19:10:17 +0300

    second way

--- Find the Remainder (8kyu).js
+++ Find the Remainder (8kyu).js
@@ -1,7 +1,13 @@
+\\ first way 
 function remainder(n, m){
     if (n==0 && m==0){
       return NaN;
     } else if (n>m){
       return n % m;
     } else return m % n;
+  }
+
+  \\ second way 
+  function remainder(a, b){  
+    return Math.max(a,b) % (Math.min(a,b) || NaN);
   }
