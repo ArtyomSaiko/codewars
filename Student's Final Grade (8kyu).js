d4a811f7ee335aa837aa78cf417e0ba5d526f6db
Artyom <artem.sok27@gmail.com>
2022-10-09 19:31:56 +0300

    fourth option

--- Student's Final Grade (8kyu).js
+++ Student's Final Grade (8kyu).js
@@ -25,3 +25,21 @@ const finalGrade = (exam, projects) => {
       exam > 50 && projects >= 2 ? 75 : 0
     )
   }
+
+  \\ fourth option
+  finalGrade = (exam, projects) => {
+    switch (true) {
+         case (exam > 90 || projects > 10):
+         return 100
+         break;
+         case (exam > 75 && projects >= 5):
+         return 90;
+         break;
+         case (exam > 50 && projects >= 2):
+         return 75
+         break;
+         case (exam <= 55 || projects <= 2):
+         return 0;
+         break;
+  }
+  }
