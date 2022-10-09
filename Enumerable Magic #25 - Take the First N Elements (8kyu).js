4e308766d7e738ccbc6eb70c5b571d47c1600053
Artyom <artem.sok27@gmail.com>
2022-10-09 18:14:40 +0300

    second way

--- Enumerable Magic #25 - Take the First N Elements (8kyu).js
+++ Enumerable Magic #25 - Take the First N Elements (8kyu).js
@@ -1,3 +1,14 @@
+\\ first way
 function take(arr, n) {
     return arr.slice(0, n);
+  }
+
+  \\ second way
+  function take(arr, n) {
+    let res = [];
+    for (let i in arr) {
+      if (i < n && i < arr.length) {
+        res.push(arr[i]);
+      }
+    } return res;
   }
