d00c1d4c31ded12e892b98fabad3fdb81ac246a1
Artyom <artem.sok27@gmail.com>
2022-10-09 18:32:19 +0300

    secind way

--- Area or Perimeter (8kyu).js
+++ Area or Perimeter (8kyu).js
@@ -1,5 +1,11 @@
+\\ first way 
 const areaOrPerimeter = function(l , w) {
     if (l==w){
       return l*w;
     } else return 2*(l+w);
+  };
+
+  \\ second way 
+  const areaOrPerimeter = function(l , w) {
+    return l == w ? l*w : 2*(l + w)
   };
