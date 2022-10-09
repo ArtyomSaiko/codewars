3c23c3553b4587d9270d34f40683f0e89ec6f1cb
Artyom <artem.sok27@gmail.com>
2022-10-09 19:43:28 +0300

    commit

---
+++ Calculate BMI (8kyu).js
@@ -0,0 +1,12 @@
+function bmi(weight, height) {
+  let bmi2 = weight/Math.pow(height, 2);
+  if (bmi2 <= 18.5){
+    return "Underweight";
+  } else if (bmi2 <= 25){
+    return "Normal";
+  } else if (bmi2 <= 30){
+    return "Overweight";
+  } else if (bmi2 > 30){
+    return "Obese";
+  }
+}
