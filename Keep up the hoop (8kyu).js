2ca2c0dd45d004e7ccf442a72ecab295f3a59d43
Artyom <artem.sok27@gmail.com>
2022-10-09 20:38:52 +0300

    second way

--- Keep up the hoop (8kyu).js
+++ Keep up the hoop (8kyu).js
@@ -1,5 +1,11 @@
+\\ first way 
 function hoopCount (n) {
     if (n >= 10){
       return "Great, now move on to tricks";
     } else return "Keep at it until you get it";
  }
+
+ \\ second way 
+ function hoopCount (n) {
+    return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+  }
