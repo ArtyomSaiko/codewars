a46fb297b13743b630247f79dd70cf940cd6ffeb
Artyom <artem.sok27@gmail.com>
2022-10-09 19:45:52 +0300

    second way

--- Calculate BMI (8kyu).js
+++ Calculate BMI (8kyu).js
@@ -1,3 +1,4 @@
+\\first way 
 function bmi(weight, height) {
   let bmi2 = weight/Math.pow(height, 2);
   if (bmi2 <= 18.5){
@@ -10,3 +11,22 @@ function bmi(weight, height) {
     return "Obese";
   }
 }
+
+\\ second way
+function bmi(weight, height) {
+  let bmi = (weight / Math.pow(height, 2));
+  switch(true){
+    case bmi <= 18.5:
+      return "Underweight";
+      break;
+    case bmi <= 25.0:
+      return "Normal";
+      break;
+    case bmi <= 30.0:
+      return "Overweight";
+      break;
+    case bmi > 30:
+      return "Obese";
+      break;
+   }
+ }
