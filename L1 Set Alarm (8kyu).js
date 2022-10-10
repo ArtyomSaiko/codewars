7689a12e49578c07e7742419d63dc51401dc77a1
Artyom <artem.sok27@gmail.com>
2022-10-10 21:15:52 +0300

    second way

--- L1 Set Alarm (8kyu).js
+++ L1 Set Alarm (8kyu).js
@@ -1,5 +1,11 @@
+\\ first way 
 function setAlarm(employed, vacation){
     if (employed == true && vacation== false){
       return true;
     } else return false;
   }
+
+  \\ second way 
+  function setAlarm(employed, vacation){
+    return employed === true && vacation === false ? true : false
+    }
