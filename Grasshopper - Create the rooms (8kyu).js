9976caa1e98b62e395d445a000ffffd64728efe1
Artyom <artem.sok27@gmail.com>
2022-10-11 21:23:03 +0300

    commit

---
+++ Grasshopper - Create the rooms (8kyu).js
@@ -0,0 +1,17 @@
+var rooms = {
+    oneRooms: {
+      name: "kitchen",
+      description: "cook food here",
+      completed: "true"
+    },
+    twoRooms: {
+      name: "bedroom",
+      description: "the owner sleeps here",
+      completed: "false"
+    },
+    threeRooms: {
+      name: "cabinet",
+      description: "a large number of documents",
+      completed: "true"
+    }
+  };
