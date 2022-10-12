371dbec5430a7f4d0e35a05eb6f7e6b8f23fbef4
Artyom <artem.sok27@gmail.com>
2022-10-12 20:43:34 +0300

    second way

--- Simple Fun  Seats in Theater (8kyu).js
+++ Simple Fun  Seats in Theater (8kyu).js
@@ -1,5 +1,11 @@
+\\ first way 
 function seatsInTheater(nCols, nRows, col, row) {
     let a = nCols - col + 1;
     let b = nRows - row;
     return a * b;
     }
+
+    \\ second way 
+    function seatsInTheater(nCols, nRows, col, row) {
+        return (nCols-col+1) * (nRows - row);  
+      }
