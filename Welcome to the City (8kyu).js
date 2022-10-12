8d914c3c687c6539dfba9ea401d5e68a38134759
Artyom <artem.sok27@gmail.com>
2022-10-12 21:24:04 +0300

    second way

--- Welcome to the City (8kyu).js
+++ Welcome to the City (8kyu).js
@@ -1,3 +1,9 @@
+\\ first way 
 function sayHello( name, city, state ) {
     return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
    }
+
+   \\ second way 
+   function sayHello( name, city, state ) {
+    return 'Hello, ' + name.join(' ') + '! Welcome to ' + city + ', ' + state + '!';
+  }
