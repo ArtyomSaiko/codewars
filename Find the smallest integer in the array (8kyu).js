78958d4ab5cca5ffa25d8ecd84be039f54070267
Artyom <artem.sok27@gmail.com>
2022-10-13 20:51:00 +0300

    commit

---
+++ Find the smallest integer in the array (8kyu).js
@@ -0,0 +1,11 @@
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+    var t;
+       for (var i=0; i < args.length; i++){
+          if(args[i] <= args[0]){
+              args[0]  = args[i];    
+              t = args[i];
+          }
+        } return t;
+    }
+  }
