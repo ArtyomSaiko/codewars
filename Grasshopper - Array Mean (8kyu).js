f201f8cdfdec8070def5f4d2213764e3f4fc7698
Artyom <artem.sok27@gmail.com>
2022-10-16 21:41:25 +0300

    second way

--- Grasshopper - Array Mean (8kyu).js
+++ Grasshopper - Array Mean (8kyu).js
@@ -1,1 +1,12 @@
+\\ first way 
 const findAverage = (nums) => nums.reduce((a, c) => a + c) / nums.length;
+
+\\second way
+var findAverage = function (nums){
+    var sum = nums.reduce(function(a, b) {
+        return a + b;
+    });
+    var avarage = sum / nums.length;
+    return avarage ;
+    
+}
