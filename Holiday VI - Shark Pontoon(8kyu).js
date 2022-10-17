d48748f496c2ee0fbdb1f9f6495ac570ed6295ad
Artyom <artem.sok27@gmail.com>
2022-10-17 17:11:31 +0300

    commit

--- Holiday VI - Shark Pontoon(8kyu).js
+++ Holiday VI - Shark Pontoon(8kyu).js
@@ -21,3 +21,16 @@ function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
       return "Shark Bait!";
     }
   }
+
+  \\ second way
+   function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    let shTime = sharkDistance/sharkSpeed;
+    let youTime = pontoonDistance/youSpeed;
+    if (dolphin == true){
+      shTime = shTime * 2;
+    } if (youTime < shTime){
+      return "Alive!";
+    } else{
+      return "Shark Bait!";
+    }
+}
