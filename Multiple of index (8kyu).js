42587cacfb4af6052f633bf34e7b1c491bad38ef
Artyom <artem.sok27@gmail.com>
2022-10-17 17:11:50 +0300

    commit

---
+++ Multiple of index (8kyu).js
@@ -0,0 +1,13 @@
+function multipleOfIndex(arr) {
+    let result = [arr[1]];
+    
+    for (let i = 2; i < arr.length; i++) {
+      if (arr[i] % i === 0) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+
+  \\ second way
+  const multipleOfIndex = arr => arr.filter((x, i) => x % i === 0);
