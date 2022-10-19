ba931d04709999bb4233c535b76640b2accfefed
Artyom <artem.sok27@gmail.com>
2022-10-19 21:02:52 +0300

    second way

--- The Wide-Mouthed frog (8kyu).js
+++ The Wide-Mouthed frog (8kyu).js
@@ -1,1 +1,6 @@
 mouthSize = animal => /alligator/i.test(animal) ? 'small': 'wide'
+
+\\ second Wway 
+function mouthSize(animal) {
+    return animal.toLowerCase() == 'alligator' ? 'small' : 'wide';
+  }
