8d17125e004981d475e3a80e257ac05b97142f28
Artyom <artem.sok27@gmail.com>
2022-10-19 20:39:11 +0300

    second way

--- Check same case (8kyu).js
+++ Check same case (8kyu).js
@@ -5,3 +5,14 @@ function sameCase(a, b) {
        return -1;
     }
   }
+
+  \\ second way 
+  function sameCase(a, b){
+    if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+       return -1
+     }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+         return 1
+     }else{
+       return 0
+     }
+   }
