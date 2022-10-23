6137437caa9e75da159702a2daeacbd6b477edba
Artyom <artem.sok27@gmail.com>
2022-10-23 21:57:51 +0300

    second way

--- Holiday VIII - Duty Free (8kyu).js
+++ Holiday VIII - Duty Free (8kyu).js
@@ -1,3 +1,8 @@
 function dutyFree(normPrice, discount, hol){
     return Math.trunc(hol/(normPrice * (discount / 100))) ;
   }
+
+  \\ second way 
+  function dutyFree(normPrice, discount, hol){
+    return(Math.floor(hol / normPrice / discount * 100))
+  }
