1895b52d451ea614105a79df50a6561ceab43e5e
Artyom <artem.sok27@gmail.com>
2022-10-24 18:25:58 +0300

    second way

--- Sum of all arguments (7kyu).js
+++ Sum of all arguments (7kyu).js
@@ -1,1 +1,10 @@
 const sum = (...arr) => arr.filter(Number).reduce((a, b) => a + b, 0);
+
+\\ second way 
+function sum() {
+    var sum = 0;
+    for(var i=0; i<arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
+  }
