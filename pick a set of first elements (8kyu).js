3d78a49c419e8668fbce7b5703b159931acbb667
Artyom <artem.sok27@gmail.com>
2022-10-25 19:13:31 +0300

    third way

--- pick a set of first elements (8kyu).js
+++ pick a set of first elements (8kyu).js
@@ -10,3 +10,8 @@ function first(arr, n=1 ) {
     function first(arr, n=1) {
         return arr.slice(0,n);
      }
+
+     \\ third way
+     function first(arr, n) {
+        return n === undefined ? arr.slice(0, 1) : arr.slice(0, n);
+      } 
