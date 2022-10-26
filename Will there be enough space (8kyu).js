ac616bf414b21ebe5cc93051220899a88941ba53
Artyom <artem.sok27@gmail.com>
2022-10-26 19:04:16 +0300

    second way

--- Will there be enough space (8kyu).js
+++ Will there be enough space (8kyu).js
@@ -1,3 +1,12 @@
 function enough(cap, on, wait) {
     return Math.max(wait + on - cap, 0);
   }
+
+  \\ second way 
+  function enough(cap, on, wait) {
+    if (on + wait < cap){
+    return 0;
+    } else {
+    return (on + wait) - cap;
+    }
+  }
