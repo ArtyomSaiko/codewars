c9eab3cd3297b97161e8e96d017d0e747177b7d0
Artyom <artem.sok27@gmail.com>
2022-10-26 20:20:02 +0300

    second way

--- Sum without highest and lowest number (8kyu).js
+++ Sum without highest and lowest number (8kyu).js
@@ -5,3 +5,19 @@ function sumArray(array) {
             ? array.reduce(sumNums, 0) - Math.min(...array) - Math.max(...array)
             : 0
 }
+
+\\ second way 
+function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var t = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        t += array[i];
+      }
+      return t;
+    }
+  }
