124365131078386d07b098bc39691059cf0c2586
Artyom <artem.sok27@gmail.com>
2022-10-30 14:38:35 +0300

    second way

--- Rock Paper Scissors (8kyu).js
+++ Rock Paper Scissors (8kyu).js
@@ -12,3 +12,14 @@ function rps(p1, p2) {
       return "Player 2 won!";
     }
   }
+ \\ second way 
+  const rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+      return "Player 1 won!";
+    }
+    else {
+      return "Player 2 won!";
+    }
+  };
