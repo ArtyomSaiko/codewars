135ebe7f701a1d7782820d5f97b5b1a5be965acb
Artyom <artem.sok27@gmail.com>
2022-11-04 19:16:42 +0300

    second way

--- Define a card suit (8kyu).js
+++ Define a card suit (8kyu).js
@@ -10,3 +10,14 @@ function defineSuit(card) {
       return 'spades';
     }
   }
+
+  \\ second way 
+  function defineSuit(card) {
+    var suit = card.substr(-1);
+    switch(suit){
+       case '♣': return 'clubs';
+       case '♦': return 'diamonds';
+       case '♥': return 'hearts';
+       default: return 'spades';
+    }
+  }
