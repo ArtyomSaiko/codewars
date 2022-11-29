47f779cb7cd39e118db7c9bd5e481f5909bd1a04
Artyom <artem.sok27@gmail.com>
2022-11-29 20:24:13 +0300

    second way

--- Fundamentals Return (8kyu).js
+++ Fundamentals Return (8kyu).js
@@ -21,3 +21,11 @@ function exponent(a,b){
 function subt(a,b){
     return a - b;
 }
+
+// second way
+const add = (a, b) => a + b;
+const subt = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
