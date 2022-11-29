856ba9d470abe2aee607bee55656c51ee68f8a63
Artyom <artem.sok27@gmail.com>
2022-11-29 20:16:50 +0300

    second way

--- 101 Dalmatians - squash the bugs, not the dogs! (8kyu).js
+++ 101 Dalmatians - squash the bugs, not the dogs! (8kyu).js
@@ -17,3 +17,16 @@ function howManyDalmatians(number) {
   
     return respond;
   }
+
+  // second way 
+  function howManyDalmatians(number){
+    if (number <= 10) {
+      return "Hardly any"
+    } else if (number <= 50) {
+      return "More than a handful!"
+    } else if (number === 101) {
+      return "101 DALMATIANS!!!"
+    } else {
+      return "Woah that's a lot of dogs!" 
+    }
+  }
