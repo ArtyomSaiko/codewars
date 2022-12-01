44de10e6fdbce1edaaaa0a816370e2f68d192cfa
Artyom <artem.sok27@gmail.com>
2022-12-01 21:00:16 +0300

    commit

--- Expressions Matter 8kyu.js
+++ Expressions Matter 8kyu.js
@@ -1,15 +1,15 @@
-\\ first way
+// first way
 function expressionMatter(a, b, c) {
     let output = [];
     output.push(a *(b + c));
     output.push(a * b * c);
     output.push(a + b * c);
     output.push((a + b) * c);
-    output.push(a+b+c);
+    output.push(a + b + c);
       return Math.max(...output);
   }
 
-  \\ second way
+  // second way
   function expressionMatter(a, b, c) {
-    return Math.max(a+b+c, a*b*c, (a+b)*c, a*(b+c));
+    return Math.max(a + b + c, a * b * c, (a + b)*c, a*(b + c));
   }
