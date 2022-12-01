7baf59c7dcc6959e0d2bcb03b9bcd6c735dcbb30
Artyom <artem.sok27@gmail.com>
2022-12-01 21:01:01 +0300

    commit

--- Find the Remainder (8kyu).js
+++ Find the Remainder (8kyu).js
@@ -1,4 +1,4 @@
-\\ first way 
+// first way 
 function remainder(n, m){
     if (n==0 && m==0){
       return NaN;
@@ -7,7 +7,7 @@ function remainder(n, m){
     } else return m % n;
   }
 
-  \\ second way 
+  // second way 
   function remainder(a, b){  
     return Math.max(a,b) % (Math.min(a,b) || NaN);
   }
