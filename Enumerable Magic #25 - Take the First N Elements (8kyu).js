02fe78a9df1111e765da572bcc2af5526e149488
Artyom <artem.sok27@gmail.com>
2022-12-01 20:59:20 +0300

    commit

--- Enumerable Magic #25 - Take the First N Elements (8kyu).js
+++ Enumerable Magic #25 - Take the First N Elements (8kyu).js
@@ -1,9 +1,9 @@
-\\ first way
+// first way
 function take(arr, n) {
     return arr.slice(0, n);
   }
 
-  \\ second way
+  // second way
   function take(arr, n) {
     let res = [];
     for (let i in arr) {
