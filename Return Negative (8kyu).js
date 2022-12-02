14247be6ce82def541b49d6990d97683def68b9d
Artyom <artem.sok27@gmail.com>
2022-12-02 20:25:34 +0300

    commit

--- Return Negative (8kyu).js
+++ Return Negative (8kyu).js
@@ -1,14 +1,17 @@
-\\sfirst way
+// first way
+
 function makeNegative(num) {
   if (num > 0) {
     return num * (-1);
   } else return num;
 }
 
-\\second way
+// second way
+
 function makeNegative(num) {
   return num < 0 ? num : -num;
 }
 
-\\third way
+// third way
+
 const makeNegative = (num) => -Math.abs(num)
