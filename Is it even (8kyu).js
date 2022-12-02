328563a53a0ec145d46985b98a4fc04ebdf26a08
Artyom <artem.sok27@gmail.com>
2022-12-02 20:19:35 +0300

    commit

--- Is it even (8kyu).js
+++ Is it even (8kyu).js
@@ -1,11 +1,11 @@
-\\ first way
+// first way
 function testEven(n) {
     if (n % 2 === 0) {
       return true;
     } else return false;
 }
 
-\\ second way
+// second way
 function testEven(n) {
   return n % 2 === 0 ? true : false;
 }
