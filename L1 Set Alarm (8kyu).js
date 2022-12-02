8301ceea411aedeb3e0a678f225fb8d3f70c2266
Artyom <artem.sok27@gmail.com>
2022-12-02 20:21:25 +0300

    commit

--- L1 Set Alarm (8kyu).js
+++ L1 Set Alarm (8kyu).js
@@ -1,11 +1,11 @@
-\\ first way 
+// first way 
 function setAlarm(employed, vacation){
     if (employed == true && vacation== false){
       return true;
     } else return false;
   }
 
-  \\ second way 
+  // second way 
   function setAlarm(employed, vacation){
     return employed === true && vacation === false ? true : false
     }
