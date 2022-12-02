975b0587c7d2f75878cc58e1c6f654633f0eba57
Artyom <artem.sok27@gmail.com>
2022-12-02 20:27:01 +0300

    commit

--- Short Long Short (8kyu).js
+++ Short Long Short (8kyu).js
@@ -1,11 +1,11 @@
-\\first way
+// first way
 function solution(a, b){
   let short = a.length < b.length ? a : b;
   let long = a.length > b.length ? a : b;
   return short + long + short;
 }
 
-\\second way
+// second way
 function solution(a, b) {
     if (a.length < b.length) {
         return a + b + a;
