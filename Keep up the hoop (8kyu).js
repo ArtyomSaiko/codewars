5aa86773cf1cacec956c6fb2630f2e848a7fb989
Artyom <artem.sok27@gmail.com>
2022-12-02 20:21:09 +0300

    commit

--- Keep up the hoop (8kyu).js
+++ Keep up the hoop (8kyu).js
@@ -1,11 +1,11 @@
-\\ first way 
+// first way 
 function hoopCount (n) {
     if (n >= 10){
       return "Great, now move on to tricks";
     } else return "Keep at it until you get it";
  }
 
- \\ second way 
+ // second way 
  function hoopCount (n) {
     return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
   }
