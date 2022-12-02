46001068f4af4391a49ba60c3f142f4e433f8c89
Artyom <artem.sok27@gmail.com>
2022-12-02 20:19:06 +0300

    commit

--- How good are you really (8kyu).js
+++ How good are you really (8kyu).js
@@ -1,9 +1,9 @@
-\\first way
+// first way
 function betterThanAverage(classPoints, yourPoints) {
   return classPoints.reduce((acc, item) => acc + item, 0) / classPoints.length < yourPoints;
   }
 
-\\second way
+// second way
 function betterThanAverage(classPoints, yourPoints) {
   var A = 0;
   for (var i = 0; i < classPoints.length; i++){
