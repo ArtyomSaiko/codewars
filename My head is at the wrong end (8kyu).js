94d461ddd4f72e2c56669c69996a103ace20bb89
Artyom <artem.sok27@gmail.com>
2022-12-02 20:22:27 +0300

    commit

--- My head is at the wrong end (8kyu).js
+++ My head is at the wrong end (8kyu).js
@@ -1,9 +1,9 @@
-\\first way
+// first way
 function fixTheMeerkat(arr) {
  return arr.reverse();
 }
 
-\\second way
+// second way
 function fixTheMeerkat(arr) {
   var end = arr.shift();
   var begin = arr.pop();
