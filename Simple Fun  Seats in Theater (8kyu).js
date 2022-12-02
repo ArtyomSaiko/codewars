c6fcb7754567713254bcfa709207e5a4e9375916
Artyom <artem.sok27@gmail.com>
2022-12-02 20:27:32 +0300

    commit

--- Simple Fun  Seats in Theater (8kyu).js
+++ Simple Fun  Seats in Theater (8kyu).js
@@ -1,11 +1,11 @@
-\\ first way 
+// first way 
 function seatsInTheater(nCols, nRows, col, row) {
     let a = nCols - col + 1;
     let b = nRows - row;
     return a * b;
     }
 
-    \\ second way 
+    // second way 
     function seatsInTheater(nCols, nRows, col, row) {
         return (nCols-col+1) * (nRows - row);  
       }
