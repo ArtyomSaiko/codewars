bc3d23a9eccba9c9313a4b26edf7b3d4d45298a6
Artyom <artem.sok27@gmail.com>
2022-12-03 11:06:13 +0300

    commit

--- Will there be enough space (8kyu).js
+++ Will there be enough space (8kyu).js
@@ -2,7 +2,7 @@ function enough(cap, on, wait) {
     return Math.max(wait + on - cap, 0);
   }
 
-  \\ second way 
+  // second way 
   function enough(cap, on, wait) {
     if (on + wait < cap){
     return 0;
