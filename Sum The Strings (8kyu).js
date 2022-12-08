5a814630e8fc05b0f596d798197de65a7d7684b9
Artyom <artem.sok27@gmail.com>
2022-12-08 22:26:15 +0300

    commit

--- Sum The Strings (8kyu).js
+++ Sum The Strings (8kyu).js
@@ -1,11 +1,11 @@
 // first way 
-function sumStr(a,b) {
+function sumStr(a, b) {
     return String(+a + +b);
   }
 
   // second way
   function sumStr(a,b) {
-    return String(Number(a)+Number(b));
+    return String(Number(a)+ Number(b));
   }
 
   // third way
